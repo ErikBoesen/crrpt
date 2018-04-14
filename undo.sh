b42967411de851902871c9382db22affe20604ec
ErikBoesen <me@erikboesen.com>
2018-04-14 12:15:26 -0400

    Save Info.plist location in undo.sh

--- undo.sh
+++ undo.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 app_root="$1"
+info="$app_root/Contents/Info.plist"
 macos="$app_root/Contents/MacOS"
 binary="$macos/$(/usr/libexec/PlistBuddy "$info" -c "Print :CFBundleExecutable")"
 
