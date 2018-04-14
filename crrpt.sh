ebc633c88b49dc26ad462ebf1fd9cdb76c9ffa7d
ErikBoesen <me@erikboesen.com>
2018-04-14 11:29:27 -0400

    Quote strings

--- crrpt.sh
+++ crrpt.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-payload=$1
-app_root=$2
-info=$app_root/Contents/Info.plist
-macos=$app_root/Contents/MacOS
-binary=$macos/$(/usr/libexec/PlistBuddy $info -c "Print :CFBundleExecutable")
+payload="$1"
+app_root="$2"
+info="$app_root/Contents/Info.plist"
+macos="$app_root/Contents/MacOS"
+binary="$macos/$(/usr/libexec/PlistBuddy $info -c "Print :CFBundleExecutable")"
 
-mv $binary ${binary}_
-cp $payload $binary
-chmod +x $binary
+mv "$binary" "${binary}_"
+cp "$payload" "$binary"
+chmod +x "$binary"
 
-echo '$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/$(basename $0)_' >> $binary
+echo '$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/$(basename $0)_' >> "$binary"
